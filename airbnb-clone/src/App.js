b7cc09063742c1146f2e70cb629a2ff6a700b907
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-27 18:33:58 -0500

    Header component created and airbnb logo put in

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Home from './components/Home'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="app">
       <h1>Airbnb clone</h1>
+      <Header />
+      <Home />
+
+      {/* Home */}
+        {/* Header */}
+        {/* Banner */}
+        {/* Cards */}
+        {/* Footer */}
+
+        {/* SearchPage */}
+          {/* ... */}
     </div>
   );
 }
