6a7fd4871189c13e7baa6721a864ae6a37d47cb2
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-29 07:58:26 -0500

    Styled Card components

--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -5,7 +5,11 @@ import '../css/card.css'
 function Card({ source, title, description, price }) {
   return (
     <div className="card">
-    <img src={source} alt=""/>
+    <img src={source} alt="" />
+    <div className="card__info">
+    <h2></h2>
+    <h4></h4>
+    </div>
     </div>
   )
 }
