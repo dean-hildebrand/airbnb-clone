8e4fceb97793ade86db9e8fe6e2032037279121c
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-30 09:46:45 -0500

    Added footer with dummy text

---
+++ airbnb-clone/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/footer.css'
+
+function Footer() {
+  return (
+    <div className="footer">
+    <p>2020 Airbnb Clone! No rights reserved. This is a demo app</p>
+    <p>Privacy | Terms | Sitemap | Company Details</p>
+    </div>
+  )
+}
+
+export default Footer
