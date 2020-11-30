b7ff33fd3e51251a619c6436cc4e7900dbf16633
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-30 09:57:01 -0500

    React router and switch statement for /search route

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,22 +1,28 @@
-import './App.css';
-import Home from './components/Home'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import "./App.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SearchPage from "./components/SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
-      {/* Home */}
-        {/* Header */}
-        {/* Banner */}
-        {/* Cards */}
-        {/* Footer */}
+      <Router>
+        <Header />
 
-        {/* SearchPage */}
-          {/* ... */}
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ airbnb-clone/src/components/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/searchPage.css'
+
+function SearchPath() {
+  return (
+    <div className="searchPage">
+    <h1>Search Page</h1>
+    </div>
+  )
+}
+
+export default SearchPath
