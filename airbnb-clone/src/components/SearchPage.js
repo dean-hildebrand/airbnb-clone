2f0e158b637c3f64c9bb9a916901d1ace93f09c7
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-30 10:06:22 -0500

    Search page component with material-ui buttons

--- airbnb-clone/src/components/SearchPage.js
+++ airbnb-clone/src/components/SearchPage.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import '../css/searchPage.css'
+import React from "react";
+import "../css/searchPage.css";
+import { Button } from "@material-ui/core";
 
-function SearchPath() {
+function SearchPage() {
   return (
     <div className="searchPage">
-    <h1>Search Page</h1>
+      <div className="searchPage__info">
+        <p>62 stays | 26 Aug to 30 Aug | 2 guests</p>
+        <h1>Stays nearby</h1>
+        <Button variant="outlined">Cancellation Flexibility</Button>
+        <Button variant="outlined">Type of place</Button>
+        <Button variant="outlined">Price</Button>
+        <Button variant="outlined">Rooms and beds</Button>
+        <Button variant="outlined">More filters</Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SearchPath
+export default SearchPage;
