b04c0f25307fe564d6dc611f78ff870bad2a2196
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-01 19:49:19 -0500

    useHistory to route onclick to different route

--- airbnb-clone/src/components/Banner.js
+++ airbnb-clone/src/components/Banner.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 import "../css/banner.css";
 import { Button } from "@material-ui/core";
-import Search from "./Search"
-
+import Search from "./Search";
+import { useHistory } from "react-router-dom";
 
 function Banner() {
-  const [showSearch, setShowSearch] = useState(false)
+  const history = useHistory();
+  const [showSearch, setShowSearch] = useState(false);
 
   return (
     <div className="banner">
       <div className="banner__search">
-      {showSearch && <Search />}
+        {showSearch && <Search />}
 
-        <Button variant="outlined" className="banner__searchButton" onClick={() => setShowSearch(!showSearch)}>
+        <Button
+          variant="outlined"
+          className="banner__searchButton"
+          onClick={() => setShowSearch(!showSearch)}
+        >
           {showSearch ? "Hide" : "Search Dates"}
         </Button>
       </div>
@@ -21,7 +26,9 @@ function Banner() {
         <h5>
           Plan a different kind of getaway to uncover the hidden gems near you
         </h5>
-        <Button variant="outlined">Explore Nearby</Button>
+        <Button onClick={() => history.push("/search")} variant="outlined">
+          Explore Nearby
+        </Button>
       </div>
     </div>
   );
